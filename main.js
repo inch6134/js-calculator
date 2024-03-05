a8c1bda9a29ab8be693d62c0e3646a41d62825ce
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-03-05 14:05:46 -0800

    Implement basic calculator operations

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // global variables
-let firstNumber = 0;
-let secondNumber = 0;
-let currentNumber = 0;
+let firstNumber = 0.00;
+let secondNumber = 0.00;
+let currentNumber = '';
 let operator = "";
-let result = 0;
-let displayValue = 0;
+let result = 0.00;
+let displayValue = '';
 const numberRegex = new RegExp("[0-9]");
 const operatorRegex = new RegExp("[+×÷-]");
+let initialized = true;
 
 // DOM elements
 const displayDOM = document.querySelector("#display");
@@ -34,7 +35,33 @@ function operate(operator, a, b) {
 }
 
 function showDisplay() {
-  displayDOM.innerHTML(displayValue);
+  if (displayValue === Infinity) {
+    displayDOM.innerHTML = 'OOPS!';
+  } else {
+    displayDOM.innerHTML =`${displayValue}`;  
+  }
+}
+
+function clear() {
+  firstNumber = 0.00;
+  secondNumber = 0.00;
+  currentNumber = '';
+  displayValue = '0';
+  operator = '';
+  result = 0.00;
+  initialized = true;
+  showDisplay();
+}
+
+function setNumber() {
+  if(initialized) {
+    firstNumber = parseFloat(currentNumber);
+    currentNumber = '';
+    initialized = false;
+  } else if(!initialized) {
+    secondNumber = parseFloat(currentNumber);
+    currentNumber = '';
+  }
 }
 
 // functions for operations
@@ -62,16 +89,27 @@ function percent () {
 // event listeners for buttons
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (numberRegex.test(button.value)) {
-      currentNumber = parseInt(button.value);
-    } else if (operatorRegex.test(button.value)) {
-      operator = button.value;
-    } else if (button.value === '%') {
+    if (numberRegex.test(button.textContent)) {
+      currentNumber += button.textContent;
+      displayValue = currentNumber;
+      showDisplay();
+    } else if (operatorRegex.test(button.textContent)) {
+      operator = button.textContent;
+      setNumber();
+    } else if (button.textContent === '.') {
+      currentNumber += button.textContent;
+      displayValue = currentNumber;
+      showDisplay();
+    } else if (button.textContent === '%') {
       percent();
-    } else if (button.value === 'C') {
+    } else if (button.textContent === 'C') {
       clear();
-    } else if (button.value === '=') {
-      operate(operator, firstNumber, secondNumber);
+    } else if (button.textContent === '=') {
+      setNumber();
+      result = Math.round(operate(operator, firstNumber, secondNumber) * 100) / 100;
+      displayValue = result;
+      showDisplay();
+      firstNumber = result;
     }
   });
 });
