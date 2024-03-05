8689f9bf1076edb18670d4ec84c80eb7ddcc61c3
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-03-05 11:45:14 -0800

    Generate event listeners for buttons

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 // global variables
 let firstNumber = 0;
 let secondNumber = 0;
+let currentNumber = 0;
 let operator = "";
 let result = 0;
 let displayValue = 0;
+const numberRegex = new RegExp("[0-9]");
+const operatorRegex = new RegExp("[+×÷-]");
 
 // DOM elements
-const displayDOM = document.querySelector('#display');
-const buttons = document.querySelectorAll('#button-container > button');
+const displayDOM = document.querySelector("#display");
+const buttons = document.querySelectorAll("#button-container > button");
 
 // utility functions
 
 function operate(operator, a, b) {
-    switch (operator) {
-        case '+':
-            return add(a,b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
 
-        case '-':
-            return subtract(a,b);
+    case "-":
+      return subtract(a, b);
 
-        case '*':
-            return multiply(a,b);
+    case "×":
+      return multiply(a, b);
 
-        case '/':
-            return divide(a,b);
-    
-        default:
-            break;
-    }
+    case "÷":
+      return divide(a, b);
+
+    default:
+      break;
+  }
 }
 
 function showDisplay() {
@@ -52,7 +55,23 @@ function divide(a, b) {
   return a / b;
 }
 
+function percent () {
+
+}
+
 // event listeners for buttons
 buttons.forEach((button) => {
-  
-})
+  button.addEventListener("click", () => {
+    if (numberRegex.test(button.value)) {
+      currentNumber = parseInt(button.value);
+    } else if (operatorRegex.test(button.value)) {
+      operator = button.value;
+    } else if (button.value === '%') {
+      percent();
+    } else if (button.value === 'C') {
+      clear();
+    } else if (button.value === '=') {
+      operate(operator, firstNumber, secondNumber);
+    }
+  });
+});
